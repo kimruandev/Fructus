9e35c2e7a592099cb4612bb5acb45867160a420f
Kim Ruan Lopes <kim.ruan@prax.net.br>
2023-09-12 15:18:42 -0300

    Hello World!

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Kim Ruan on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
