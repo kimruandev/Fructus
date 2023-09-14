f432162b1493211fdb4718084ad34a7c9560878f
Kim Ruan Lopes <kim.ruan@prax.net.br>
2023-09-14 16:37:50 -0300

    Seetings label view

---
+++ Fructus/View/SettingsLabelView.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsLabelView.swift
+//  Fructus
+//
+//  Created by Kim Ruan on 14/09/23.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    // MARK: - PROPERTIES
+    
+    var labelText: String
+    var labelImage: String
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        HStack {
+            Text(labelText.uppercased()).fontWeight(.bold)
+            Spacer()
+            Image(systemName: labelImage)
+        } //: HSTACK
+    }
+}
+
+// MARK: - PREVIEW
+
+struct SettingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView(labelText: "Fructus", labelImage: "info.circl")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
