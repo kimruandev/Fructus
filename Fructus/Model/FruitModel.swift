b8b0ee7887765551d606f23820d71aa6690c3551
Kim Ruan Lopes <kim.ruan@prax.net.br>
2023-09-14 01:55:28 -0300

    Data model and fetching data

---
+++ Fructus/Model/FruitModel.swift
@@ -0,0 +1,20 @@
+//
+//  FruitModel.swift
+//  Fructus
+//
+//  Created by Kim Ruan on 14/09/23.
+//
+
+import SwiftUI
+
+// MARK: - FRUITS DATA MODEL
+
+struct Fruit: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String]
+}
